1eb9338a91b06d3b92b0001bb9b6fa60608f20e3
Albert Magadan <novalis.janm@gmail.com>
2020-11-15 21:45:37 -0700

    creación inicial del tema. assets, font, imágenes, boostrap, test js

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+
+function init_template(){
+    add_theme_support( 'post-thumbnails');
+    add_theme_support( 'title-tag');
+
+}
+
+add_action('after_setup_theme','init_template');
+
+function assets(){
+	wp_register_style('bootstrap','https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css','','4.5.0','all');
+	wp_register_style('montserrat','https://fonts.googleapis.com/css2?family=Montserrat&display=swap','','1.0','all');
+	
+	wp_enqueue_style('estilos',get_stylesheet_uri(), array('bootstrap','montserrat'),'1.0','all');
+	
+	wp_register_script('popper','https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js','','1.16.0',true);
+	
+	wp_enqueue_script('boostrap','https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array('jquery','popper'),'4.4.1',true);
+	wp_enqueue_script('custom',get_template_directory_uri().'/assets/js/custom.js','',1.0,true)
+	
+}
+
+add_action('wp_enqueue_scripts','assets');
+
+    
+
+    
---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head() ?>
+</head>
+<body>
+	<h1>
+		Sitio en construcción...
+	</h1>
+	Autor: José Alberto Núñez Magadán<br>
+    Aspiring Data Scientist
+    
+     
+<!-- 
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <img src="
+                <?/*php echo get_template_directroy_uri()*/?>
+                /assets/img/logo.png" alt="logo">
+            </div>
+        </div>
+    </div>
+
+</header>
+ -->
